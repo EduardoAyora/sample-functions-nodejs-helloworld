eaa54b493160eafd572ed95b8dff107115c88f57
Eduardo Ayora Ochoa <49033198+EduardoAyora@users.noreply.github.com>
2022-10-24 01:23:07 -0500

    Update hello.js

--- packages/sample/hello/hello.js
+++ packages/sample/hello/hello.js
@@ -1,8 +1,8 @@
 const fetch = require('node-fetch');
 
 async function main(args) {
-  //const res = await fetch('http://143.198.108.130')
-  const res = await fetch('http://10.124.0.2')
+  const res = await fetch('http://143.198.108.130')
+  //const res = await fetch('http://10.124.0.2')
   return res.json()
 }
 
